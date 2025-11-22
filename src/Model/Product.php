34b7f2749978ad92da77c03e1d49323cabbe8610
sandy.schirmer <sandy.schirmer@zep.de>
2025-11-22 10:35:20 +0100

    pint config update

--- src/Model/Product.php
+++ src/Model/Product.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace App\Model;
 
-final class Product
+final readonly class Product
 {
     public function __construct(
         private string $sku,
         private int $priceInCents,
     ) {
-    }
-
-    public function getSku(): string
-    {
-        return $this->sku;
     }
 
     public function getPriceInCents(): int
